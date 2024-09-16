614706007f175abb5ca84a75502a4c142c9e45c6
Arnob <abamit551@gmail.com>
2024-09-16 18:20:19 +0600

    first commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.NameTb = new System.Windows.Forms.TextBox();
+            this.IdTb = new System.Windows.Forms.TextBox();
+            this.EmailTb = new System.Windows.Forms.TextBox();
+            this.AgeTb = new System.Windows.Forms.TextBox();
+            this.PhoneTb = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.DGV = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.BgCb = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(65, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(395, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(24, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(687, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(65, 113);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Age";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(666, 113);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Blood Group";
+            // 
+            // NameTb
+            // 
+            this.NameTb.Location = new System.Drawing.Point(12, 66);
+            this.NameTb.Name = "NameTb";
+            this.NameTb.Size = new System.Drawing.Size(164, 20);
+            this.NameTb.TabIndex = 5;
+            // 
+            // IdTb
+            // 
+            this.IdTb.Location = new System.Drawing.Point(322, 66);
+            this.IdTb.Name = "IdTb";
+            this.IdTb.Size = new System.Drawing.Size(164, 20);
+            this.IdTb.TabIndex = 6;
+            // 
+            // EmailTb
+            // 
+            this.EmailTb.Location = new System.Drawing.Point(624, 66);
+            this.EmailTb.Name = "EmailTb";
+            this.EmailTb.Size = new System.Drawing.Size(164, 20);
+            this.EmailTb.TabIndex = 7;
+            // 
+            // AgeTb
+            // 
+            this.AgeTb.Location = new System.Drawing.Point(12, 145);
+            this.AgeTb.Name = "AgeTb";
+            this.AgeTb.Size = new System.Drawing.Size(164, 20);
+            this.AgeTb.TabIndex = 8;
+            // 
+            // PhoneTb
+            // 
+            this.PhoneTb.Location = new System.Drawing.Point(322, 145);
+            this.PhoneTb.Name = "PhoneTb";
+            this.PhoneTb.Size = new System.Drawing.Size(164, 20);
+            this.PhoneTb.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(379, 113);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 19);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Phone";
+            // 
+            // DGV
+            // 
+            this.DGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DGV.Location = new System.Drawing.Point(69, 198);
+            this.DGV.Name = "DGV";
+            this.DGV.Size = new System.Drawing.Size(645, 223);
+            this.DGV.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(238, 450);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(466, 450);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // BgCb
+            // 
+            this.BgCb.FormattingEnabled = true;
+            this.BgCb.Items.AddRange(new object[] {
+            "A+",
+            "A-",
+            "B+",
+            "B-",
+            "O+",
+            "O-",
+            "AB+",
+            "AB-"});
+            this.BgCb.Location = new System.Drawing.Point(624, 145);
+            this.BgCb.Name = "BgCb";
+            this.BgCb.Size = new System.Drawing.Size(164, 21);
+            this.BgCb.TabIndex = 17;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.ForestGreen;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(358, 450);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Edit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 517);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.BgCb);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DGV);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.PhoneTb);
+            this.Controls.Add(this.AgeTb);
+            this.Controls.Add(this.EmailTb);
+            this.Controls.Add(this.IdTb);
+            this.Controls.Add(this.NameTb);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.DGV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox NameTb;
+        private System.Windows.Forms.TextBox IdTb;
+        private System.Windows.Forms.TextBox EmailTb;
+        private System.Windows.Forms.TextBox AgeTb;
+        private System.Windows.Forms.TextBox PhoneTb;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView DGV;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox BgCb;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            populate();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\arnob\OneDrive\Documents\DemoDB.mdf;Integrated Security=True;Connect Timeout=30;");
+
+        private void Reset()
+        {
+            IdTb.Text = "";
+            NameTb.Text = "";
+            EmailTb.Text = "";
+            AgeTb.Text = "";
+            PhoneTb.Text = "";
+            BgCb.SelectedIndex = -1;           
+        }
+
+        private void populate()
+        {
+            con.Open();
+            string query = "select * from InfoTable";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            SqlCommandBuilder builder = new SqlCommandBuilder(sda);
+            var ds = new DataSet();
+            sda.Fill(ds);
+            DGV.DataSource = ds.Tables[0];
+            con.Close();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (IdTb.Text == "" || NameTb.Text == "" || EmailTb.Text == "" || AgeTb.Text == "" ||  PhoneTb.Text == "" || BgCb.SelectedIndex == -1)
+            {
+                MessageBox.Show("Missing Information");
+            }
+
+            else
+            {
+                try
+                {
+                    string query = "insert into InfoTable values('" + IdTb.Text + "', '" + NameTb.Text + "', '" + EmailTb.Text + "', '" + AgeTb.Text + "', '" + PhoneTb.Text + "', '" + BgCb.SelectedItem.ToString() + "')";
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Information Successfully Saved");
+                    con.Close();
+                    populate();
+                    Reset();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Delete from InfoTable where Id=@Id", con);
+            cmd.Parameters.AddWithValue("@Id", IdTb.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Information Successfully Deleted");
+            populate();
+            Reset();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Update InfoTable set Id=@Id, Name=@Name, Email=@Email, Age=@Age, Phone=@Phone, Blood_Group=@Blood_Group", con);
+            cmd.Parameters.AddWithValue("@Id", IdTb.Text);
+            cmd.Parameters.AddWithValue("@Name", NameTb.Text);
+            cmd.Parameters.AddWithValue("@Email", EmailTb.Text);
+            cmd.Parameters.AddWithValue("@Age", int.Parse(AgeTb.Text));
+            cmd.Parameters.AddWithValue("@Phone", int.Parse(PhoneTb.Text));
+            cmd.Parameters.AddWithValue("@Blood_Group", BgCb.Text);
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Information Successfully Updated");
+            populate();
+            Reset();
+        }
+    }
+}
